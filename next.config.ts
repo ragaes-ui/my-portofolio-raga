6e3b0f11d0dbdf87efb50cc502c7c0505a341da1
ragaes-ui <ragaesa75@gmail.com>
2026-02-15 21:11:47 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's0.wordpress.com', // Mengizinkan API Screenshot WordPress
+      },
+      {
+        protocol: 'https',
+        hostname: 'rsudcibinong.id', // Jaga-jaga kalau mau pakai gambar manual dari sana
+      },
+    ],
+  },
 };
 
 export default nextConfig;
